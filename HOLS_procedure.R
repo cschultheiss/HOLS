9cf2e38c3be0a5f6372a22a48460b2d93e901f31
Christoph Schultheiss <cschulth@math.ethz.ch>
2021-02-11 14:48:43 +0100

    HOLS utils

---
+++ HOLS_procedure.R
@@ -0,0 +1,48 @@
+HOLS.check <- function(x, y, use.Lasso = FALSE, center = FALSE,
+                       standardize = FALSE, return.z = FALSE, return.w = FALSE, ...){
+  n <- dim(x)[1]
+  p <- dim(x)[2]
+  if (length(y) != n) stop("Dimensions do not match")
+  if (p >= n && !use.Lasso) 
+    stop("use.Lasso is set to FALSE, this is not okay for high-dimensional data")
+  beta.OLS <- numeric(p)
+  beta.HOLS <- numeric(p)
+  sd.scale <- numeric(p)
+  w <- matrix(NA, nrow = n, ncol = p)
+  if (use.Lasso){
+    lp <- lasso.proj(x, y, standardize = FALSE, return.Z = TRUE, ...)
+    beta.OLS <- lp$bhat
+    z <- lp$Z
+    for (j in 1:p){
+      zj <- as.vector(z[, j])
+      w[,j] <- wj <- y- x[, -j] %*% lp$betahat[-j]
+      beta.HOLS[j] <- crossprod(zj^3, wj)/(crossprod(zj^3, x[,j]))
+      sscale <- (zj^3/(crossprod(zj^3, x[,j]))[1,1]-zj/(crossprod(zj, x[,j]))[1,1])
+      sd.scale[j] <- sqrt(sum(sscale^2))
+      sigma.hat <- lp$sigmahat
+    }
+  } else {
+    xtx.inv <- solve(crossprod(x))
+    d <- diag(xtx.inv)
+    gamma <- xtx.inv / d
+    z <- x %*% t(gamma)
+    for (j in 1:p){
+      xtx.sub.inv <- xtx.inv[-j, -j] - tcrossprod(xtx.inv[-j, j])/d[j]
+      P_j <- diag(n) - x[,-j] %*% xtx.sub.inv %*% t(x[,-j])
+      w[,j] <- wj <- P_j %*% y
+      zj <- as.vector(z[,j])
+      beta.OLS[j] <- crossprod(zj, wj) / norm(zj, "2")^2
+      beta.HOLS[j] <- crossprod(zj^3, wj) / norm(zj^2, "2")^2
+      sd.scale[j] <- sqrt((t(zj^3) %*% P_j %*% zj^3) / sum(zj^4)^2 - 1/sum(zj^2))
+    }
+    den <- n - p2
+    if (center) den <- den - 1
+    sigma.hat <- sqrt(sum((y - x2 %*% beta.OLS)^2) / (n - p2))
+  }
+  pval <- 2 * pnorm(abs(beta.OLS - beta.HOLS) / sd.scale / sigma.hat, lower.tail = FALSE)
+  out <- list(beta.OLS = beta.OLS, beta.HOLS = beta.HOLS,
+              sd.scale = sd.scale, sigma.hat = sigma.hat, pval = pval)
+  if (return.z) out$z <- z
+  if (return.w) out$w <- w
+  return(out)
+}
