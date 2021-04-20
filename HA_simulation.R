6326ba13e47bb07aa8e65852055ad98fa901c9f1
Christoph Schultheiss <cschulth@stat.math.ethz.ch>
2021-04-20 15:48:27 +0200

    x vs x^2

--- HA_simulation.R
+++ HA_simulation.R
@@ -39,8 +39,8 @@ progress <- function(n, tag) {
 opts <- list(progress = progress)
 
 n.vec <- c(1e2, 1e3, 1e4, 1e5, 1e6, 1e7)
-p <- 7
-p2 <- 7
+p <- 1
+p2 <- 1
 # rho <- 0.6
 # Cov <- toeplitz(rho^(seq(0, p - 1)))
 # Cov <- Cov * solve(Cov)[5,5]
@@ -48,7 +48,7 @@ p2 <- 7
 # ind <- sel.index
 beta <- rep(0, p)
 # beta[sel.index] <- 1
-sigma <- 2
+sigma <- 0
 alpha <- sqrt(2.5)
 
 RNGkind("L'Ecuyer-CMRG")
@@ -67,23 +67,25 @@ for (n in n.vec) {
   tic()
   res<-foreach(gu = 1:nsim, .combine = rbind,
                .packages = c("MASS", "Matrix", "hdi", "MultiRNG", "tictoc"), .options.snow = opts) %dorng%{
-    x1 <- rt(n, df = 7) / sqrt(1.4)
-    x2 <- sqrt(0.5) * x1 + sqrt(0.5) * rnorm(n)
-    x3 <- rt(n, df = 7) / sqrt(1.4)
-    x4 <- 0.5 * x2 + 0.5 * x3 + sqrt(0.5) * runif(n, -sqrt(3), sqrt(3))
-    x5 <- rt(n, df = 7) / sqrt(1.4)
-    x6 <- 0.5 * x4 + 0.5 * x5 + sqrt(0.5) * rnorm(n)
-    x7 <- sqrt(0.5) * x6 + sqrt(0.5) * rnorm(n)
-    
-    x <- eval(parse(text =paste("cbind(", paste("x", 1:7, sep="", collapse = ","), ")")))
+    # x1 <- rt(n, df = 7) / sqrt(1.4)
+    # x2 <- sqrt(0.5) * x1 + sqrt(0.5) * rnorm(n)
+    # x3 <- rt(n, df = 7) / sqrt(1.4)
+    # x4 <- 0.5 * x2 + 0.5 * x3 + sqrt(0.5) * runif(n, -sqrt(3), sqrt(3))
+    # x5 <- rt(n, df = 7) / sqrt(1.4)
+    # x6 <- 0.5 * x4 + 0.5 * x5 + sqrt(0.5) * rnorm(n)
+    # x7 <- sqrt(0.5) * x6 + sqrt(0.5) * rnorm(n)
+    # 
+    # x <- eval(parse(text =paste("cbind(", paste("x", 1:7, sep="", collapse = ","), ")")))
     
     # H <- rexp(n, rate = sqrt(2)) * sample(c(-1,1), n, TRUE)
     # 
     # x[, 3] <- x[, 3] + H
+                 
+    x <- matrix(rnorm(n), n)
     
     x.sub <- x[, 1:p2]
     y0 <- x%*%beta
-    y.true <- y0 + x[, 4]^3
+    y.true <- y0 + x[, 1]^2 - 1 
     y <- y.true + sigma * rnorm(n)
     
     out <- list()
@@ -116,6 +118,6 @@ for (n in n.vec) {
   resname <- paste0("results n=", n, " ", format(Sys.time(), "%d-%b-%Y %H.%M"))
   if (save) save(simulation, file = paste("results/", newdir, "/", resname, ".RData", sep = ""))
   
-  print(simulation.summary(simulation, variables = c(1, 2, 3, 4)))
+  print(simulation.summary(simulation))
 }
 
