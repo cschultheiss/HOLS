f70b36cf3cfbaae99ba102eed4b672bba4ba44e2
Christoph Schultheiss <cschulth@stat.math.ethz.ch>
2021-05-27 17:57:44 +0200

    Figure for artifical nonlinearity

--- figures.R
+++ figures.R
@@ -24,3 +24,23 @@ matplot(zs[, 1], zs[, -1], log ="xy", xlab = "n", ylab = "Average z-statistics",
 legend("topleft", col = 1:length(var.ind), legend = paste("x", var.ind, sep=""), pch = 1)
 lines(zs[, 1], sqrt(zs[, 1]) * max(zs[4, -1]) / sqrt(zs[4, 1]), lty = 2)
 lines(zs[, 1], sqrt(zs[, 1]) * max(zs[4, 3]) / sqrt(zs[4, 1]), lty = 2)
+
+folder <- "results/27-May-2021 17.35"
+flz <- list.files(folder)
+
+
+j <- 0
+for (file in flz) {
+  j <- j + 1
+  load(paste(folder, "/", file, sep = ""))
+  if (j == 1) {
+    p <- sum(colnames(simulation$res) == "beta.FOLS")
+    zs <- matrix(NA, length(flz), p + 1)
+  }
+  zs[j, 1] <- simulation$n
+  zs[j, -1] <- apply(abs(simulation$res[,which(colnames(simulation$res) == "t.val")]), 2, mean)
+}
+
+var.ind <- 1:p
+matplot(zs[, 1], zs[, -1], log ="xy", xlab = "n", ylab = "Average z-statistics", pch = 1)
+legend("topleft", col = 1:length(var.ind), legend = paste("x", var.ind, sep=""), pch = 1, horiz = TRUE)
