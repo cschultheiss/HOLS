9a6fa1c4d2d317ab7afa38edde98ac8ca41fb29f
Christoph Schultheiss <cschulth@stat.math.ethz.ch>
2021-07-02 11:59:33 +0200

    Add plot function

--- cyto/cyto_analysis.R
+++ cyto/cyto_analysis.R
@@ -9,10 +9,19 @@ for(i in 1:n.tot) {
   else ivn[i] = cytometryContinuous$ivn[[i]]
 }
 
-cC.HOLS <- function(predictor, response, env){
+cyto.HOLS <- function(predictor, response, env){
   if (env == "all") env <- unique(ivn)
   ss <- which(ivn %in% env)
   x <- cytometryContinuous$data[ss, predictor]
   y <- cytometryContinuous$data[ss, response]
   HOLS.check(x, y)
+}
+
+cyto.plot <- function(predictor, response, env){
+  if (env == "all") env <- unique(ivn)
+  ss <- which(ivn %in% env)
+  x <- cytometryContinuous$data[ss, predictor]
+  y <- cytometryContinuous$data[ss, response]
+  plot(x, y, xlab = predictor, ylab = response,
+       main = paste(env, sep = " ", collapse = ", "))
 }
