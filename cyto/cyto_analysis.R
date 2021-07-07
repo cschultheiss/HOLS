447e17f752fdbe25669dc2fbb379bd439032470a
Christoph Schultheiss <cschulth@stat.math.ethz.ch>
2021-07-07 17:19:53 +0200

    Colour environments

--- cyto/cyto_analysis.R
+++ cyto/cyto_analysis.R
@@ -9,19 +9,33 @@ for(i in 1:n.tot) {
   else ivn[i] = cytometryContinuous$ivn[[i]]
 }
 
-cyto.HOLS <- function(predictor, response, env){
+cyto.HOLS <- function(predictor, response, env, nolog = FALSE){
   if (env == "all") env <- unique(ivn)
   ss <- which(ivn %in% env)
   x <- cytometryContinuous$data[ss, predictor]
   y <- cytometryContinuous$data[ss, response]
+  if (nolog) {
+    x <- exp(x)
+    y <- exp(y)
+  }
   HOLS.check(x, y)
 }
 
-cyto.plot <- function(predictor, response, env){
+cyto.plot <- function(predictor, response, env, nolog = FALSE){
   if (env == "all") env <- unique(ivn)
   ss <- which(ivn %in% env)
+  cols <- 1 : length(env)
+  all.col <- numeric(length(ss))
+  for (i in 1:length(ss)){
+    all.col[i] = which(env == ivn[ss][i])
+  }
   x <- cytometryContinuous$data[ss, predictor]
   y <- cytometryContinuous$data[ss, response]
+  if (nolog) {
+    x <- exp(x)
+    y <- exp(y)
+  }
   plot(x, y, xlab = predictor, ylab = response,
-       main = paste(env, sep = " ", collapse = ", "))
+       main = paste(env, sep = " ", collapse = ", "), col = all.col)
+  legend("topleft", legend = env, col = cols, pch = 1)
 }
