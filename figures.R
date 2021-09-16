9e340bf5f1bac65e887e91bef937d990024b8ee5
Christoph Schultheiss <cschulth@stat.math.ethz.ch>
2021-09-16 11:27:29 +0200

    Block independent

--- figures.R
+++ figures.R
@@ -1,7 +1,7 @@
 require(latex2exp)
 
-folder <- "results/Gaussian eps_x2"
-savefolder <- "Figures/Gaussian eps_x"
+folder <- "results/block independent"
+savefolder <- "Figures/block independent"
 flz <- list.files(folder)
 grepf <- function(str) grepl("+07", str)
 flz <- flz[which(!sapply(flz, grepf))]
@@ -25,16 +25,15 @@ for (file in flz) {
   
 }
 
-# var.ind <- c(1, 7, 9, 13, 14, 20, 22, 26)
-var.ind <- 13:17
+var.ind <- c(1, 7, 9, 13, 14, 20, 22, 26)
 pp <- length(var.ind)
-var.ind.label <- 13:17
+var.ind.label <- var.ind
 # nmat <- matrix(rep(zs[,1], p), ncol = p)
 # alln <- nmat[,1]
 # scat <- seq(0.95, 1.05, 0.02)
 # nmat.scat <- tcrossprod(alln, scat)
 labels <- eval(parse(text = paste("c(", paste("TeX('$X_{", var.ind.label, "}$')", sep = "", collapse = ","), ")")))
-ord <- matrix(1:pp, nrow = 2, ncol = 3, byrow = T)
+ord <- matrix(1:pp, nrow = 2, ncol = 4, byrow = T)
 plotfac <- 4
 pointfrac <- 0.8
 cx <- 0.75
@@ -44,16 +43,16 @@ cx <- 0.75
 
 
 
-conf.ind <- 14:16
-unconf.ind <- (1:30)[-conf.ind]
+conf.ind <- 1:13
+unconf.ind <- (1:26)[-conf.ind]
 # beta0 <- rep(0, 6)
 # beta.OLS <- sqrt(2.5) * c(0, -1/3, 2/3, 0, 0, 0)
-beta0 <- rep(0, 30)
-beta.OLS <- c(rep(0, 13), -0.3487806,  0.7905694, -0.3487806, rep(0, 14))
-# beta0 <- rep(0, 26)
-# beta.OLS <- c(0.3046434, -0.08445936, 0.01745333, 0.02742916, 0.1939396, 0.09398516,
-#               -0.5261166, -0.12003, -0.2272695, 0.03484701, 0.03762141, 0.05294573, 0.1150129,
-#               rep(0, 13))
+# beta0 <- rep(0, 30)
+# beta.OLS <- c(rep(0, 13), -0.3487806,  0.7905694, -0.3487806, rep(0, 14))
+beta0 <- rep(0, 26)
+beta.OLS <- c(0.3046434, -0.08445936, 0.01745333, 0.02742916, 0.1939396, 0.09398516,
+              -0.5261166, -0.12003, -0.2272695, 0.03484701, 0.03762141, 0.05294573, 0.1150129,
+              rep(0, 13))
 dbeta <- beta0 - beta.OLS
 max.unconf <- matrix(NA, 200, length(flz))
 min.conf <- matrix(NA, 200, length(flz)) 
@@ -111,11 +110,11 @@ par(mfrow = c(1,2))
 par(xpd = TRUE)
 matplot(zs[, 1], zs[, var.ind + 1], log ="xy", xlab = "n",
         ylab = "Average absolute z-statistics", # main = TeX("Confounding onto $X_{1}$ and $X_{7}$"),
-        pch = 1:pp, col = (1:7)[-5], lwd = 2)
-legend("topleft", inset = c(0, -0), ncol = 3, legend = labels[ord][1:pp],
-       pch = (1:pp)[ord], col = (1:7)[-5][ord], pt.lwd = 2)
+        pch = 1:pp, col = rep(1:4, 2), lwd = 2)
+legend("topleft", inset = c(0, -0.15), ncol = 4, legend = labels[ord][1:pp],
+       pch = (1:pp)[ord], col = rep(1:4, 2)[ord], pt.lwd = 2)
 par(xpd = FALSE)
-which.line <- (14:15)
+which.line <- c(1, 7, 9, 13)
 for (j in which.line){
   lines(zs[, 1], sqrt(zs[, 1]) * zs[4, j + 1] / sqrt(zs[4, 1]), lty = 2)
 }
