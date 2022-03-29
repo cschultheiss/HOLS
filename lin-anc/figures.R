fb323462b2c38d018b5a0f6c161558e484316159
Christoph Schultheiss <christoph.schultheiss@stat.math.ethz.ch>
2022-03-29 18:12:07 +0200

    Set to 1 higher p-val => no causal contradiction

--- lin-anc/figures.R
+++ lin-anc/figures.R
@@ -6,6 +6,19 @@ flz <- list.files(folder)
 grepf <- function(str) grepl("+07", str)
 flz <- flz[which(!sapply(flz, grepf))]
 
+set1 <- function(pvs){
+  pv <- as.numeric(pvs)
+  p <- sqrt(length(pv))
+  if (abs(p - round(p)) > 1e-5) stop("Bad column number")
+  mat <- as.matrix(matrix(pv, nrow = p))
+  mat[mat >= t(mat)] <- 1
+  pvs[] <- c(mat)
+  return(pvs)
+}
+
+p <- 7
+nf <- length(flz)
+
 pmat <- matrix(FALSE, nrow = p, ncol = p)
 diag(pmat) <- NA
 pmat[1, 2] <- pmat [2, 4] <- pmat[3, 4] <- pmat[4, 6] <- pmat[5, 6] <- pmat[6, 7] <- TRUE
@@ -23,16 +36,18 @@ for (j in 1:p){
 }
 
 len <- 200 * length(which(!ancmat))
-TPR2 <- TPR <- TAR <- matrix(NA, nrow = len + 1, ncol = length(flz))
-lg.perf <- matrix(NA, nrow = 3, ncol = length(flz))
-laa.perf <- matrix(NA, nrow = 4, ncol = length(flz))
+TPR2 <- TPR <- TAR <- matrix(NA, nrow = len + 1, ncol = nf)
+lg.perf <- matrix(NA, nrow = 3, ncol = nf)
+laa.perf <- matrix(NA, nrow = 4, ncol = nf)
 j <- 0
 for (file in flz) {
   j <- j + 1
   load(paste(folder, "/", file, sep = ""))
-  ancs <- simulation$res[,1:p^2][,which(ancmat)]
-  pars <- simulation$res[,1:p^2][,which(pmat)]
-  non.ancs <- simulation$res[,1:p^2][,which(!ancmat)]
+  pvs <- simulation$res[,1:p^2]
+  pvs <- t(apply(pvs, 1, set1))
+  ancs <- pvs[,which(ancmat)]
+  pars <- pvs[,which(pmat)]
+  non.ancs <- pvs[,which(!ancmat)]
   lims <- c(0, sort(non.ancs))
   TAR[,j] <- sapply(lims, function(lim) mean(ancs < lim))
   TPR[,j] <- sapply(lims, function(lim) mean(pars < lim))
@@ -47,13 +62,13 @@ for (file in flz) {
 }
 
 matplot((0:len)/len, TAR, type = "l")
-points(laa.perf[1,], laa.perf[2,], col = 1:4)
+points(laa.perf[1,], laa.perf[2,], col = 1:nf)
 matplot((0:len)/len, TPR, type = "l")
-points(laa.perf[1,], laa.perf[3,], col = 1:4)
-points(lg.perf[1,], lg.perf[2,], col = 1:4, pch = 2)
+points(laa.perf[1,], laa.perf[3,], col = 1:nf)
+points(lg.perf[1,], lg.perf[2,], col = 1:nf, pch = 2)
 matplot((0:len)/len, TPR2, type = "l")
-points(laa.perf[1,], laa.perf[4,], col = 1:4)
-points(lg.perf[1,], lg.perf[3,], col = 1:4, pch = 2)
+points(laa.perf[1,], laa.perf[4,], col = 1:nf)
+points(lg.perf[1,], lg.perf[3,], col = 1:nf, pch = 2)
 
 
 len <- 200
@@ -64,15 +79,21 @@ j <- 0
 for (file in flz) {
   j <- j + 1
   load(paste(folder, "/", file, sep = ""))
-  ancs <- simulation$res[,1:p^2][,which(ancmat)]
-  pars <- simulation$res[,1:p^2][,which(pmat)]
-  non.ancs <- simulation$res[,1:p^2][,which(!ancmat)]
+  pvs <- simulation$res[,1:p^2]
+  # pvs <- t(apply(pvs, 1, set1))
+  pvs[which(is.na(ancmat))] <- 1
+  pvs.sub <- pvs[,which(!is.na(ancmat))]
+  pvs.corr <- t(apply(pvs.sub, 1, function(ps) p.adjust(ps, "holm")))
+  pvs[,which(!is.na(ancmat))] <- pvs.corr
+  ancs <- pvs[,which(ancmat)]
+  pars <- pvs[,1:p^2][,which(pmat)]
+  non.ancs <- pvs[,1:p^2][,which(!ancmat)]
   lims <- c(0, sort(apply(non.ancs, 1, min)))
   TAR[,j] <- sapply(lims, function(lim) mean(ancs < lim))
   TPR[,j] <- sapply(lims, function(lim) mean(pars < lim))
   TPR2[,j] <- sapply(lims, function(lim) mean(pars[,-6] < lim))
   
-  alpha <- 0.05 #/ (p^2 - p)
+  alpha <- 0.05
   lg.pars <- simulation$res[, p^2 + (1:p^2)][,which(pmat)]
   lg.non.ancs <- simulation$res[, p^2 + (1:p^2)][,which(!ancmat)]
   lg.perf[, j] <- c(mean(apply(lg.non.ancs, 1, max)), mean(lg.pars), mean(lg.pars[,-6]))
@@ -80,11 +101,11 @@ for (file in flz) {
                      mean(pars < alpha), mean(pars[,-6] < alpha))
 }
 
-matplot((0:len)/len, TAR, type = "l")
-points(laa.perf[1,], laa.perf[2,], col = 1:4)
-matplot((0:len)/len, TPR, type = "l")
-points(laa.perf[1,], laa.perf[3,], col = 1:4)
-points(lg.perf[1,], lg.perf[2,], col = 1:4, pch = 2)
-matplot((0:len)/len, TPR2, type = "l")
-points(laa.perf[1,], laa.perf[4,], col = 1:4)
-points(lg.perf[1,], lg.perf[3,], col = 1:4, pch = 2)
+matplot((0:len)/len, TAR, type = "l", ylim = c(0, 1))
+points(laa.perf[1,], laa.perf[2,], col = 1:nf)
+matplot((0:len)/len, TPR, type = "l", ylim = c(0, 1))
+points(laa.perf[1,], laa.perf[3,], col = 1:nf)
+points(lg.perf[1,], lg.perf[2,], col = 1:nf, pch = 2)
+matplot((0:len)/len, TPR2, type = "l", ylim = c(0, 1))
+points(laa.perf[1,], laa.perf[4,], col = 1:nf)
+points(lg.perf[1,], lg.perf[3,], col = 1:nf, pch = 2)
