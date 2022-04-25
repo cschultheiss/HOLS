21d53511684630dc27ab79b3b076428a557e00c5
Christoph Schultheiss <christoph.schultheiss@stat.math.ethz.ch>
2022-04-25 13:57:38 +0200

    Add holm.uncut

--- lin-anc/lin-anc-functions.R
+++ lin-anc/lin-anc-functions.R
@@ -159,3 +159,11 @@ holm.matrix <- function(pv, cut = TRUE){
   }
   pv
 }
+
+holm.uncut <- function(pv){
+  p <- length(pv)
+  i <- seq_len(p)
+  o <- order(pv)
+  ro <- order(o)
+  cummax((p + 1L - i) * pv[o])[ro]
+}
