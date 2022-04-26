fdc6b37d68711ce294d0dcd241117326930810f4
Christoph Schultheiss <christoph.schultheiss@stat.math.ethz.ch>
2022-04-26 09:42:58 +0200

    Change unif and t; Gaussian x1

--- lin-anc/anc_simulation.R
+++ lin-anc/anc_simulation.R
@@ -73,8 +73,8 @@ for (n in n.vec) {
   res<-foreach(gu = 1:nsim, .combine = rbind,
                .packages = c("MASS", "Matrix", "hdi", "MultiRNG", "tictoc", "pcalg"), .options.snow = opts) %dorng%{
                  
-       psi <- cbind(rt(n, 7) / sqrt(1.4), rt(n, 7) / sqrt(1.4), runif(n, -sqrt(3), sqrt(3)),
-                    rt(n, 7) / sqrt(1.4), runif(n, -sqrt(3), sqrt(3)), rnorm(n), rnorm(n))
+       psi <- cbind(rnorm(n), runif(n, -sqrt(3), sqrt(3)), rt(n, 7) / sqrt(1.4),
+                    runif(n, -sqrt(3), sqrt(3)), runif(n, -sqrt(3), sqrt(3)), rnorm(n), rnorm(n))
        
        a <- 0.7
        x1 <- psi[, 1]
