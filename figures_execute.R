44bc287656bc884ed0572088f4f8cce1236c8fd1
Christoph Schultheiss <christoph.schultheiss@stat.math.ethz.ch>
2022-08-11 17:02:48 +0200

    Move execution to another file

---
+++ figures_execute.R
@@ -0,0 +1,15 @@
+source("figures_fun.R")
+
+HA_plot("results/SEM missing x3", exclude.chars = "+07", z.plot.ind = 1:6, z.plot.ind.label = (1:7)[-3], conf.ind = 2:3, all.ind = 1:6,
+        beta0 = rep(0, 6), beta.OLS = sqrt(2.5) * c(0, -1/3, 2/3, 0, 0, 0), zlims.var = (0.1) * (1.1^(0:60)), which.line = 2:3,
+        groups = list(2, 3, -c(2,3)), group.labels = c("2", "4", "U"))
+
+HA_plot("results/block independent", z.plot.ind = c(1, 7, 9, 13, 14, 20, 22, 26), conf.ind = 1:13, all.ind = 1:26,
+        beta0 = rep(0, 26), beta.OLS = c(0.3046434, -0.08445936, 0.01745333, 0.02742916, 0.1939396, 0.09398516,
+                                         -0.5261166, -0.12003, -0.2272695, 0.03484701, 0.03762141, 0.05294573, 0.1150129,
+                                         rep(0, 13)), zlims.var = (0.1) * (1.1^(0:60)), which.line = c(1, 7, 9, 13), colgroups = 2,
+        groups = list(1, 9, 14:26), group.labels = c("1", "9", "U"))
+
+HA_plot("results/SEM HD", exclude.chars = "+07", z.plot = FALSE, groups = list(2, 3, -c(2,3)), group.labels = c("3", "4", "U"), hd = TRUE)
+
+H0_plot("results/mix-Gauss null.RData")
