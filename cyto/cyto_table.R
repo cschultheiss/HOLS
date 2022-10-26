9a2fa5655a46150558e6d4cae4a153d1bcbaa44e
Christoph Schultheiss <christoph.schultheiss@stat.math.ethz.ch>
2022-10-26 11:32:27 +0200

    Add comments; remove superfluous variables

--- cyto/cyto_table.R
+++ cyto/cyto_table.R
@@ -3,34 +3,42 @@ source('cyto/cyto_functions.R')
 options(digits = 2)
 
 folder <- "Protein-signal"
+# get all files
 flz <- list.files(folder)
 flz2 <- character(0)
 for (i in 1:length(flz)){
+  # get all file corresponding to an environment
   if (!is.na(as.numeric(substr(flz[i], 1 ,1)))) flz2 <- c(flz2, flz[i])
 }
 all.env <- numeric(0)
 for(file in flz2){
+  # read environment number from file
   all.env <- c(all.env, as.numeric(gsub( " .*.", "", file )))
 }
+# sort files by environment
 flz2 <- flz2[order(all.env)]
 
 i <- 0
 for (file in flz2){
   i <- i + 1
+  # read next envrionment's data
   dat.temp <- read_excel(paste(folder, "/", file, sep = ""))
   dat.temp$env <- as.numeric(gsub( " .*.", "", file ))
   colnames(dat.temp) <- tolower(colnames(dat.temp))
+  # append the data
   if (i == 1){
     dat <- dat.temp
   } else {
     dat <- rbind(dat, dat.temp)
   }
 }
+# use a different column name that R can handle
 colnames(dat)[which(colnames(dat) == "p44/42")] <- "p44_42"
 
 vars <- colnames(dat)[-12]
 parents <- matrix(FALSE, length(vars), length(vars))
 rownames(parents) <- colnames(parents) <- vars
+# encode the consensus network in a matrix
 parents["praf", c("pkc", "pka")] <- TRUE
 parents["pmek", c("praf")] <- TRUE
 parents["plcg", c("pip3")] <- TRUE
@@ -42,62 +50,61 @@ parents["pkc", c("plcg")] <- TRUE
 parents["p38", c("pka", "pkc")] <- TRUE
 parents["pjnk", c("pkc", "pka")] <- TRUE
 
-
-env.map <- c("NA", "NA", "akt", "pkc", "pip2", "mek", "NA", "pkc", "pka")
+# use 8 environments following previous projects
 envs <- (1:9)[-2]
 
 # using consensus network
 log <- TRUE
+# store everything in a list over environments
 all.analysis <- list()
 for (env in envs){
-  env.a <- env
-  if (length(env) > 1) env.a <- paste(env, collapse = ", ")
   all.analysis[[env]] <- list()
+  # matrices to store linear model and HOLS p-values
   pval.lm <- matrix(NA, length(vars), length(vars))
   rownames(pval.lm) <- colnames(pval.lm) <- vars
   pval.HOLS <- pval.lm
   for (var in vars){
-    cat(var)
-    cat(":  ")
+    # use parents as predictors
     preds <- names(which(parents[var,]))
-    cat (preds)
-    cat("   ")
     if (length(preds) > 0){
       form <- eval(paste(var, "~", paste(preds, collapse = " + ")))
+      # fit linear model
       fit <- lm(form, data = log(dat[dat$env %in% env,]))
-      hc <- cyto.HOLS(preds, var, env, log = log)
-      cat(" beta.OLS:", hc$beta.OLS)
-      cat(" pval: ", hc$pval)
       pval.lm[var, preds] <- summary(fit)$coefficients[-1, 4]
+      # fit HOLS
+      hc <- cyto.HOLS(preds, var, env, log = log)
       pval.HOLS[var, preds] <- hc$pval
     }
-    cat("\n")
   }
   
-  pval.max <- pval.lm
-  pval.max.filtered <- pval.max
-  pval.max.filtered[pval.HOLS < 0.05] <- NA
-  all.analysis[[env.a]]$pval.lm <- pval.lm
-  all.analysis[[env.a]]$pval.HOLS <- pval.HOLS
-  all.analysis[[env.a]]$pval.max <- pval.max
-  all.analysis[[env.a]]$pval.max.filtered <- pval.max.filtered
+  # filter for edges that pass the HOLS check
+  pval.lm.filtered <- pval.lm
+  pval.lm.filtered[pval.HOLS < 0.05] <- NA
+  all.analysis[[env]]$pval.lm <- pval.lm
+  all.analysis[[env]]$pval.HOLS <- pval.HOLS
+  all.analysis[[env]]$pval.lm <- pval.lm
+  all.analysis[[env]]$pval.lm.filtered <- pval.lm.filtered
 }
 
-pas <- paste("all.analysis[[", envs, "]]$pval.max.filtered", collapse = ", ", sep = "")
+# get minimum over the environments of pval.lm.filtered for each edge
+pas <- paste("all.analysis[[", envs, "]]$pval.lm.filtered", collapse = ", ", sep = "")
 pval.min <- eval(parse(text = paste("pmin(", pas, ", na.rm = TRUE)", sep="")))
 
-sums <- eval(parse(text = paste("1*(!is.na(all.analysis[[", envs, "]]$pval.max))", sep = "", collapse = " + ")))
-sums.filtered <- eval(parse(text = paste("1*(!is.na(all.analysis[[", envs, "]]$pval.max.filtered))", sep = "", collapse = " + ")))
-pvals <- pval.min[!is.na(pval.min)]
-sums <- sums[sums.filtered > 0]
-sums.filtered <- sums.filtered[sums.filtered > 0]
+# count number of environments passing HOLS
+sums <- eval(parse(text = paste("1*(!is.na(all.analysis[[", envs, "]]$pval.lm.filtered))", sep = "", collapse = " + ")))
+# store to vectors
+pvals.reported <- pval.min[!is.na(pval.min)]
+sums.reported <- sums[sums > 0]
 ma <- map(which(!is.na(pval.min), arr.ind = TRUE))
-ord <- order(pvals)
+# order by p-value
+ord <- order(pvals.reported)
 
-for (i in 1:length(sums)){
+for (i in 1:length(sums.reported)){
+  # get edge with LaTeX name
   cat(paste(latex_name(ma[ord[i],2:1]), collapse = " $\\rightarrow$ "))
   cat(" & ")
-  cat(sums.filtered[ord[i]], " & ", pvals[ord[i]])
+  # report number of environments and minimum p-value
+  cat(sums.reported[ord[i]], " & ", pvals.reported[ord[i]])
   cat(paste("\\", "\\", sep=""))
   cat("\n")
 }
