013116f74c3aee1cfc1e61c5143766b2d4a13b17
Christoph Schultheiss <christoph.schultheiss@stat.math.ethz.ch>
2022-11-09 15:54:36 +0100

    Comments to mixgauss

--- mixgauss_simulation.R
+++ mixgauss_simulation.R
@@ -17,7 +17,9 @@ commit <- revparse_single(revision = "HEAD")
 print(paste("Run on commit", commit$sha, 'i.e.:', commit$summary))
 
 save <- TRUE
+# create unique filename based on sample size and time
 resname <- paste0("results ", format(Sys.time(), "%d-%b-%Y %H.%M"))
+# number of simulations
 nsim <- 200
 progress <- function(n, tag) {
   mod <- 16
@@ -34,16 +36,20 @@ cl<-makeSOCKcluster(16)
 
 registerDoSNOW(cl)
 
+# number of observations
 n <- 100
+# number of variables high-dimensional
 p <- 200
+# number of variables low-dimensiona
 p2 <- 30
+# correlation for toeplitz covariance structure
 rho <- 0.6
-Cov <- toeplitz(rho^(seq(0, p - 1)))
-sqrt.Cov <- sqrtm(Cov)
+# nonzero regression coefficients
 ind <- c(1, 5, 10, 15, 20)
-samp.mix <- function(n) rnorm(n) * sample(c(rep(sqrt(0.5), 2), sqrt(2)), n, TRUE)
 beta <- rep(0, p)
 beta[ind] <- 1
+# sample from mixture of Gaussian
+samp.mix <- function(n) rnorm(n) * sample(c(rep(sqrt(0.5), 2), sqrt(2)), n, TRUE)
 
 RNGkind("L'Ecuyer-CMRG")
 set.seed(42)
@@ -52,15 +58,20 @@ tic()
 res<-foreach(gu = 1:nsim, .combine = rbind,
              .packages = c("MASS", "Matrix", "hdi", "glmnet", "MultiRNG", "tictoc"), .options.snow = opts) %dorng%{
 
+               # independent noise terms
                psi <- matrix(samp.mix(n * p), nrow = n)
                
+               # generate x with Toeplitz structure
                x <- matrix(NA, nrow = n, ncol = p)
                x[ ,1] <- psi[, 1]
                for (j in 2:p){
                  x[ ,j] <- rho * x[ , j - 1] + sqrt(1- rho^2) * psi[, j]
                }
                
+               # less variables for low-dimensional HOLS
                x2 <- x[, 1:p2]
+               
+               # generate y
                y.true <- x%*%beta
                eps <- samp.mix(n)
                y <- y.true + eps
@@ -68,12 +79,14 @@ res<-foreach(gu = 1:nsim, .combine = rbind,
                # low-dimensional
                out <- list()
                
-               # low-dimensional
+               # low-dimensional HOLS check
                out$low.dim <- HOLS.check(x2, y, simulated.pval = TRUE, center = TRUE)
                
-               # high-dimensional
+               # find nodewise residuals first
                lp <- lasso.proj(x, y, standardize = FALSE, return.Z = TRUE, do.ZnZ = FALSE)
+               # lazy version of high-dimensional HOLS without debiasing z^3
                out$high.dim <- HOLS.check(x, y, use.Lasso = TRUE, lasso.proj.out = lp)
+               # full version of high-dimensional HOLS
                out$high.dim.new <- HOLS.check(x, y, use.Lasso = TRUE, lasso.proj.out = lp,
                                               debias.z3 = TRUE, return.changed = TRUE, do.ZnZ = FALSE)
                out                              
@@ -81,6 +94,7 @@ res<-foreach(gu = 1:nsim, .combine = rbind,
 toc()
 stopCluster(cl)
 
+# store output list to matrix
 res.low <- matrix(unlist(res[, "low.dim"]), byrow = TRUE, nrow = nsim)
 colnames(res.low) <- c(rep("beta.OLS", p2), rep("beta.HOLS", p2),
                        rep("sd.scale", p2), "sigma.hat",
@@ -96,9 +110,13 @@ colnames(res.high.new) <- c(rep("beta.OLS", p), rep("beta.HOLS", p),
                             rep("sd.scale", p), "sigma.hat",
                             rep("pval", p), rep("pval.corr", p), "changed")
 
+# store output quantities, random seed, commit
 simulation <- list(low.dim = res.low, high.dim = res.high, high.dim.new = res.high.new,
                    r.seed = attr(res, "rng"), "commit" = commit)
 
+# save the output file
 if (save) save(simulation, file = paste("results/", resname, ".RData", sep = ""))
 
+# quick summary of simulation: with which fraction is global null rejected based on minimum p-value
+# and, only for low-dimensional case, based on sum p-value and based on simulated p-values;
 simulation.summary(simulation)
