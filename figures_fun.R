4f2c8911f51b1258d3d162fd8cdd06005a054fb9
Christoph Schultheiss <christoph.schultheiss@stat.math.ethz.ch>
2022-11-10 10:08:47 +0100

    Comments for HA-plot

--- figures_fun.R
+++ figures_fun.R
@@ -1,6 +1,5 @@
 require(latex2exp)
 
-# wrapper function to generate plots apart from the global null
 HA_plot <- function(folder, exclude.chars = NULL, z.plot = TRUE, z.plot.ind = NULL, z.plot.ind.label = z.plot.ind, 
                     conf.ind = NULL, beta0 = NULL, beta.OLS = NULL, zlims.var = (0.1) * (1.1^(0:60)),
                     colgroups = 1, which.line = NULL, ecdf.plot = TRUE, groups = NULL, group.labels = NULL, hd = FALSE){
@@ -116,27 +115,35 @@ HA_plot <- function(folder, exclude.chars = NULL, z.plot = TRUE, z.plot.ind = NU
     
     # define labels based on used sample sizes
     labels.rec <- eval(parse(text = paste("c(", paste("TeX('$n=10^", log10(zs[,1]), "$')", sep = "", collapse = ","), ")")))
+    # number of colours
     n.col <- ceiling(pp / colgroups)
     par(mfrow = c(1,2))
     par(xpd = TRUE)
+    # plot of z-statistics
     matplot(zs[, 1], zs[, z.plot.ind + 1], log ="xy", xlab = "n",
             ylab = "Average absolute z-statistics",
             pch = 1:pp, col = rep((1:(n.col + 1))[-5], colgroups), lwd = 2)
     legend("topleft", inset = c(0, -0.15), ncol = ceiling(pp / 2), legend = labels[ord][1:pp],
            pch = (1:pp)[ord], col = rep((1:(n.col + 1))[-5], each = colgroups), pt.lwd = 2)
     par(xpd = FALSE)
+    # add root-n growth for defined variables
     for (j in which.line){
       lines(zs[, 1], sqrt(zs[, 1]) * zs[4, j + 1] / sqrt(zs[4, 1]), lty = 2)
     }
+    # plot recovery of perfect set U
     matplot(zlims.var, true.model.var, lty = 1:nn, type = "l", log = "x",
             main = "Perfect recovery of U", xlab = "Threshold on the absolute z-statistics",
             ylab = "Empirical probability", col = (1:(nn + 1))[-5], lwd = 2)
     legend('topleft', col = (1:(nn + 1))[-5], lwd = 2, legend = labels.rec, lty = 1:5)
     
+    # plot  probability of at least one false inclusion versus intersection size
+    # limits for U.size defined such that correspond to 0 up to nsim runs with false inclusion
     matplot((0:nsim)/nsim, U.size, type = "l", lty = 1:nn,
             xlab = TeX("1-$P(\\hat{U}\\subseteq U)$"),
             ylab = "Average intersection size", col = (1:(nn + 1))[-5], lwd = 2)
     
+    # plot l1-fraction of missed confounding versus average intersection size
+    # both are calculated at predefined limits for z
     matplot(diff.var/sum(abs(dbeta)), size.var, type = "l", lty = 1:nn,
             xlab = TeX("$||\\beta^{OLS}_{\\hat{U}} - \\beta_{\\hat{U}}||_1 / ||\\beta^{OLS} - \\beta ||_1$"),
             ylab = "Average intersection size", col = (1:(nn + 1))[-5], lwd = 2)
@@ -146,32 +153,49 @@ HA_plot <- function(folder, exclude.chars = NULL, z.plot = TRUE, z.plot.ind = NU
   
   if(ecdf.plot){
     plo <- function(data){
+      # sub-function to possible make several plots
+      # labels for the plot
       var.labels.tex <- eval(parse(text = paste("c(", paste("TeX('ECDF of p-values for $X_", group.labels, "$')", sep = "", collapse = ","), ")")))
+      # quantiles to look at for manually creating plots of ecdf
       qs <- seq(0, 1, 0.01)
-      last <- nn
+      # colors for the plot
       cols <- (1:(nn + 1))[-5]
       par(mfrow = c(1, length(groups)))
       i <- 0
       for (group in groups) {
+        # which group do we look at
         i <- i + 1
         j <- 0
+        # where to stop, possibly ignore larger sample sizes if perfect right angle is obtained
         last.new <- nn
+        # sample sizes
+        ns <- numeric(nn)
         for (file in flz) {
+          # which file do we look at
           j <- j + 1
           load(paste(folder, "/", file, sep = ""))
+          # sample size
+          ns[j] <- simulation$n
+          # extract row p-values from saved file
           pv <- simulation[[data]][,which(colnames(simulation[[data]]) == "pval")]
           if (j == 1)
+            # plot the ecdf
             plot(qs, ecdf(pv[,group])(qs), col = cols[j], xlim = c(0,1), type = "l", lwd = 2, lty = j,
                  xlab = "p", ylab ="Fn(p)", main = var.labels.tex[i])
           else if (last.new >= j)
+            # add the next ecdf
             lines(qs, ecdf(pv[,group])(qs), col = cols[j], lwd = 2, lty = j)
-          if (last.new == last && ecdf(pv[,group])(qs[2])  == 1) last.new <- j
+          # if right angle reached, ignore larger sample sizes
+          if (last.new == nn && ecdf(pv[,group])(qs[2])  == 1) last.new <- j
         }
-        labels.rec <- eval(parse(text = paste("c(", paste("TeX('$n=10^", 2:7, "$')", sep = "", collapse = ","), ")")))
+        # create labels based on sample size
+        labels.rec <- eval(parse(text = paste("c(", paste("TeX('$n=10^", log10(ns), "$')", sep = "", collapse = ","), ")")))
         legend('bottomright', col = cols, lwd = 2, legend = labels.rec[1:last.new], lty = 1:last.new)
       }
     }
+    # plot the results for low-dimensional HOLS
     plo("low.dim")
+    # plot results for high-dimensional HOLS if applicable
     if(hd) plo("high.dim.new")
   }
 }
