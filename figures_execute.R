07cf15debb77c5344f2b795698d86d2f8d5bf503
Christoph Schultheiss <christoph.schultheiss@stat.math.ethz.ch>
2022-11-15 11:58:37 +0100

    More comments to execute

--- figures_execute.R
+++ figures_execute.R
@@ -3,7 +3,10 @@ source("figures_fun.R")
 # creates Figures 2, 3, 6
 source("SEM_simulation.R")
 # run the simulation and store information on save location that was automatically created based on time
+# the result files contain the output of the HOLS check, the latest commit, the respective sample size
+# and the random seeds for each simulation run in order to recreate individual results if desired
 SEM_folder <- SEM_simulation()
+# plot the results as in the paper
 HA_plot(SEM_folder, z.plot.ind = 1:6, z.plot.ind.label = (1:7)[-3], conf.ind = 2:3,
         beta0 = rep(0, 6), beta.OLS = sqrt(2.5) * c(0, -1/3, 2/3, 0, 0, 0), zlims.var = (0.1) * (1.1^(0:60)), which.line = 2:3,
         groups = list(2, 3, -c(2,3)), group.labels = c("2", "4", "U"))
@@ -11,20 +14,31 @@ HA_plot(SEM_folder, z.plot.ind = 1:6, z.plot.ind.label = (1:7)[-3], conf.ind = 2
 # creates Figure 5
 source("mixgauss_simulation.R")
 # run the simulation and store information on save location that was automatically created based on time
+# the result files contain the output of the HOLS check (low-dimensional and high-dimensional),
+# the latest commit
+# and the random seeds for each simulation run in order to recreate individual results if desired
 H0_file <- mixgauss_simulation()
+# plot the results as in the paper
 H0_plot(H0_file)
 
 # creates Figures 9, 8
 source("SEMHD_simulation.R")
 # run the simulation and store information on save location that was automatically created based on time
+# the result files contain the output of the HOLS check (low-dimensional and high-dimensional),
+# the latest commit, the respective sample size
+# and the random seeds for each simulation run in order to recreate individual results if desired
 SEMHD_folder <- SEMHD_simulation()
+# plot the results as in the paper
 HA_plot(SEMHD_folder, z.plot = FALSE, groups = list(2, 3, -c(2,3)),
         group.labels = c("3", "4", "U"), hd = TRUE)
 
 # creates Figures 10, 12, 11
 source("block_simulation.R")
 # run the simulation and store information on save location that was automatically created based on time
+# the result files contain the output of the HOLS check, the latest commit, the respective sample size
+# and the random seeds for each simulation run in order to recreate individual results if desired
 block_folder <- block_simulation()
+# plot the results as in the paper
 HA_plot(block_folder, z.plot.ind = c(1, 7, 9, 13, 14, 20, 22, 26), conf.ind = 1:13,
         beta0 = rep(0, 26), beta.OLS = c(0.3046434, -0.08445936, 0.01745333, 0.02742916, 0.1939396, 0.09398516,
                                          -0.5261166, -0.12003, -0.2272695, 0.03484701, 0.03762141, 0.05294573, 0.1150129,
